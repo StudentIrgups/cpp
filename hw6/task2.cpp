71bef63c38c75bed4fedb998852be327d1b94fa9
StudentIrgups <beznosov.1992@gmail.com>
2025-12-09 15:09:04 +0300

    task2:

--- hw6/task2.cpp
+++ hw6/task2.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 
+int my_pow(int value, int power) {
+	int result = 1;
+	for (int i = 0; i < power; ++i)
+			result *= value;
+	return result;
+}
+
+void print(int value, int power) {
+	std::cout << value << " в степени " << power << " = " << my_pow(value, power) << std::endl;
+}
 int main() {
-	int mass[] = {3, 5, -3, 4, 345, 1231, 236, 2332, 1123, 234, 6643};
-	std::cout << "Массив: ";
-	int size = sizeof(mass)/sizeof(mass[0]);
-	int max =-2147483648, min = 2147483647;
-
-	for (int i = 0; i < size; ++i) {
-		std::cout << mass[i] << " ";
-		if (mass[i] > max)
-			max = mass[i];
-		if (mass[i] < min)
-			min = mass[i];
-	}
-
-	std::cout << std::endl << "Минимальный элемент: " << min << std::endl;
-	std::cout << "Максимальный элемент: " << max << std::endl;
-
-    return 0;
+    print(5,2);
+	print(3,3);
+	print(4,4);
+	return 0;
 }
