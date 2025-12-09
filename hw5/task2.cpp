775be3b005e552e034580a05a8e14b5f9f07da2c
StudentIrgups <beznosov.1992@gmail.com>
2025-12-09 14:06:10 +0300

    task2

--- hw5/task2.cpp
+++ hw5/task2.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Введите целое число: ";
-    int num{};
-    std::cin >> num;
+	int mass[] = {3, 5, -3, 4, 345, 1231, 236, 2332, 1123, 234, 6643};
+	std::cout << "Массив: ";
+	int size = sizeof(mass)/sizeof(mass[0]);
+	int max =-2147483648, min = 2147483647;
 
-    int sum{};
-    while (num != 0) {
-        sum += num % 10;
-        num /= 10;
-    }
+	for (int i = 0; i < size; ++i) {
+		std::cout << mass[i] << " ";
+		if (mass[i] > max)
+			max = mass[i];
+		if (mass[i] < min)
+			min = mass[i];
+	}
 
-    std::cout << "Сумма цифр: " << sum << "\n\n\n";
+	std::cout << std::endl << "Минимальный элемент: " << min << std::endl;
+	std::cout << "Максимальный элемент: " << max << std::endl;
 
     return 0;
 }
