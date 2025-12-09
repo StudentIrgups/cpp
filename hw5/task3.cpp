67953e9182deb7199753497f2f652ccfa1f12384
StudentIrgups <beznosov.1992@gmail.com>
2025-12-09 14:25:44 +0300

    task3

--- hw5/task3.cpp
+++ hw5/task3.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Введите целое число: ";
-    int num{};
-    std::cin >> num;
-    for (int i = 1; i < 11; i++) {
-        std::cout << i << " * " << num << " = " << i * num << std::endl;
-    }
-    std::cout << "\n\n";
-    return 0;
+    int mass[3][6] = {
+		{ 2, 5, -9, 213, 3, 1 },
+		{ 3, 562, 36, 1, 0, 1 },
+		{ 1, 12, 35, 215, 23, 99 }
+	}; 
+	int rows = sizeof(mass) / sizeof(mass[0]);
+	int columns = sizeof(mass[0]) / sizeof(mass[0][0]);
+
+	int max = -2147483648, min = 2147483647;
+	int mini = 0, minj = 0, maxi = 0, maxj = 0;
+
+	std::cout << "Массив: \n";
+
+	for (int i = 0; i < rows; ++i) {
+		for (int j = 0; j < columns; ++j) {
+			if (mass[i][j] > max) {
+				max = mass[i][j];
+				maxi = i;
+				maxj = j;
+			}
+			if (mass[i][j] < min) {					
+				min = mass[i][j];
+				mini = i;
+				minj = j;
+			}
+			std::cout << mass[i][j] << "\t";
+		}
+		std::cout << std::endl;
+	}
+	std::cout << "Индекс минимального элемента: " << mini << " " << minj << std::endl;
+	std::cout << "Индекс максммального элемента: " << maxi << " " << maxj << std::endl;
+	return 0;
 }
