106f190d3f5849060f75e2e3490a5e568316adf0
StudentIrgups <beznosov.1992@gmail.com>
2025-12-10 14:55:06 +0300

    fix

--- hw5/task4.cpp
+++ hw5/task4.cpp
@@ -9,10 +9,10 @@ int main() {
 	}
 	std::cout << std::endl;
 
-	for (int i = 0; i < size; ++i) {
-		for (int j = i+1; j < size; ++j) {
+	for (int i = size-1; i > -1; --i) {
+		for (int j = i-1; j > -1; --j) {
 			if (mass[i] > mass[j]) {
-				int tmp = mass[j];
+		int tmp = mass[j];
 				mass[j] = mass[i];
 				mass[i] = tmp;
 			}
