282c7aa43182d6cfaa33ad0490a8f77917a392c6
StudentIrgups <beznosov.1992@gmail.com>
2025-12-11 17:26:55 +0900

    beg hw11

--- hw11/task2.cpp
+++ hw11/task2.cpp
@@ -2,7 +2,16 @@
 
 int main(int argc, char** argv)
 {
-	
+    std::string word = "";
+    do {
+        if (word.length() > 0)
+            std::cout << "Неправильно!" << std::endl;
+        std::cout << "Угадайте слово: ";
+        std::cin >> word;
+
+    } while (word != "Арбуз");
+    
+    std::cout << "Правильно! Вы победили! Загаданное слово — " + word << std::endl;
 }
 
 
