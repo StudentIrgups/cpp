eac7f28e8cc62d2fbcae3723350e7d32c3e7b7c0
StudentIrgups <beznosov.1992@gmail.com>
2025-12-30 17:33:25 +0900

    coursework

--- courseWork/solve.cpp
+++ courseWork/solve.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <fstream>
+int toInt(std::string data);
+int toInt(char data);
+void massInit(std::string** arr, int n, int m);
+std::string** create_two_dim_array(int n, int m);
 
 int main() {
     std::ifstream inFile("in.txt");
@@ -7,13 +11,15 @@ int main() {
         int n{}, m{};
         inFile >> n;
         inFile >> m;
-        std::string mass[n][m]{"-"};
+        std::string** mass = create_two_dim_array(n,m);
+        massInit(mass, n, m);
         std::string s{};
+         
          while (std::getline(inFile, s)) {
-             if (s.length() == 3) {
-                 int i = s[0] - 1, j = s[2] - 1;
-                 mass[i][j] = "*";
-             } 
+            if (s.length() == 3) {
+                int i = toInt(s[0]) - 1, j = toInt(s[2]) - 1;
+                mass[i][j] = "*";
+            } 
         } 
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j < m; ++j) {
@@ -24,4 +30,33 @@ int main() {
         inFile.close();
     }
     return 0;
+}
+
+int toInt(std::string data) {
+    int n = 0;
+    for (int i = 0; i < data.length(); ++i) {
+        n *= 10;
+        n += data[i] - '0';        
+    }
+    return n;
+}
+
+int toInt(char data) {
+    return data - '0';
+}
+
+void massInit(std::string** arr, int n, int m) {
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            arr[i][j] = "-";
+        }
+    }
+}
+
+std::string** create_two_dim_array(int n, int m){
+    std::string** arr = new std::string*[n];
+    for (int i = 0; i < n; ++i) {
+        arr[i] = new std::string[m];
+    }
+    return arr;
 }
