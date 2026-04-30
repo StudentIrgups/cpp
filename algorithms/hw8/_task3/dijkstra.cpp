cc13ea414bb9cfe26c61f7c0702a02aa766449ec
StudentIrgups <beznosov.1992@gmail.com>
2026-04-30 11:56:24 +0900

    algosHw8

--- algorithms/hw8/_task3/dijkstra.cpp
+++ algorithms/hw8/_task3/dijkstra.cpp
@@ -12,22 +12,24 @@ void dijkstra(int ** graph, int n, int s, int * done) {
 
     while (!hp->is_empty()) {
         int curr = hp->extract_min();
-        if (curr == -1 || visited[curr]) continue;
+        if (curr == -1 || visited[curr]) 
+            continue;
         visited[curr] = true;
         done[curr] = d[curr];
 
         for (int v = 0; v < n; ++v) {
-            if (graph[curr][v] == 0 || visited[v]) continue;
+            if (graph[curr][v] == 0 || visited[v]) 
+                continue;
 
             // Защита от переполнения
-            if (d[curr] == INT32_MAX) continue;
+            if (d[curr] == INT32_MAX) 
+                continue;
 
             if (d[curr] + graph[curr][v] < d[v]) {
                 d[v] = d[curr] + graph[curr][v];                
                 hp->add({v, d[v]});
             }
-        }
-        
+        }        
     }
 
     delete[] d;
