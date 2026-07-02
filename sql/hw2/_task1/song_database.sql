11ae3aa17f00fca4b0247236d1a1eb85619e9846
StudentIrgups <beznosov.1992@gmail.com>
2026-07-02 16:12:03 +0900

    sqlHw2

--- sql/hw2/_task1/song_database.sql
+++ sql/hw2/_task1/song_database.sql
@@ -36,7 +36,13 @@ create  table if not exists track (
 );
 
 create table if not exists collection (
+	id serial primary key,
 	track_id integer not null references track(id),
 	album_id integer not null references album(id),
 	name varchar(100)
 );
+
+create table if not exists trackCollectino(
+	track_id integer not null references track(id),
+	collection_id integer not null references collection(id)
+); 
